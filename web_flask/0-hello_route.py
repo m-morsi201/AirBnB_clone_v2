b3bac5d1ca2d737f726f929e83812d39ae3111a4
Muhamad Morsy <muhammadmorsy5@gmail.com>
2024-05-19 14:16:56 -0700

    edit pycodestyle for file web_flask/0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""
-	Function that return Hello
-	"""	
+    """
+    Function that return Hello
+    """
 
-	return 'Hello HBNB!'
 
+return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
